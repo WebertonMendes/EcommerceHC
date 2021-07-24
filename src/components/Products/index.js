3fdbc61e3baf447886de01e807a678b9bf64f248
Weberton Mendes <weberton.mendes@hotmail.com>
2021-07-24 15:50:21 -0300

    first commit

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-import api from "../../services/api";
+import api from "../../../api/";
 
 import bannerProduct from "../../assets/images/banner-product.jpg";
 import BasketIcon from "../../assets/icons/basket.svg";
