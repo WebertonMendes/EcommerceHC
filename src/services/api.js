5ad9eed517c8ee012b3fa389b232f9f9c2df116b
Weberton Mendes <weberton.mendes@hotmail.com>
2021-07-28 08:11:15 -0300

    JSON-API-Fake

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // baseURL: 'http://localhost:3333',
     baseURL: 'https://my-json-server.typicode.com/WebertonMendes/EcommerceHC'
 })
 
